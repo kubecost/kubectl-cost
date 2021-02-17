3a48c8b01ba00dd1d851e3d95fe8ae5534b6cf7f
Michael Dresser <michael@kubecost.com>
2021-02-17 10:00:31 -0500

    Fixed table sort to be numeric

--- pkg/cmd/output.go
+++ pkg/cmd/output.go
@@ -137,7 +137,7 @@ func writeAllocationTable(out io.Writer, allocationType string, allocations map[
 	t.SortBy([]table.SortBy{
 		{
 			Name: "Total Cost (All)",
-			Mode: table.Dsc,
+			Mode: table.DscNumeric,
 		},
 	})
 
@@ -397,10 +397,9 @@ func writeAggregationRateTable(out io.Writer, aggs map[string]query.Aggregation,
 	t.AppendHeader(headerRow)
 
 	sortByConfig := []table.SortBy{}
-
 	sortByConfig = append(sortByConfig, table.SortBy{
 		Name: "Monthly Rate (All)",
-		Mode: table.Dsc,
+		Mode: table.DscNumeric,
 	})
 
 	t.SortBy(sortByConfig)
