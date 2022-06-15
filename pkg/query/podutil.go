b6d44b67fec7bb40b7b3f70b68fe98d2cab67686
Michael Dresser <michaelmdresser@gmail.com>
2022-06-15 11:44:56 -0400

    Only port-forward to Ready Pods
    Tested on a cluster with spot nodes, which causes the
    kubecost-cost-analyzer pod to occasionally die in a Completed state,
    which cannot serve requests. This change makes kubectl-cost continue to
    work correctly in that environment.

---
+++ pkg/query/podutil.go
@@ -0,0 +1,48 @@
+package query
+
+import (
+	v1 "k8s.io/api/core/v1"
+)
+
+// Code in this file taken from:  https://github.com/kubernetes/kubernetes/blob/b1e2af45e4ff126fcbcffa2e66ed36502103be7b/pkg/api/v1/pod/util.go#L299-L361 to avoid importing all of k/k
+
+// IsPodReady returns true if a pod is ready; false otherwise.
+func isPodReady(pod *v1.Pod) bool {
+	return isPodReadyConditionTrue(pod.Status)
+}
+
+// IsPodReadyConditionTrue returns true if a pod is ready; false otherwise.
+func isPodReadyConditionTrue(status v1.PodStatus) bool {
+	condition := getPodReadyCondition(status)
+	return condition != nil && condition.Status == v1.ConditionTrue
+}
+
+// GetPodReadyCondition extracts the pod ready condition from the given status and returns that.
+// Returns nil if the condition is not present.
+func getPodReadyCondition(status v1.PodStatus) *v1.PodCondition {
+	_, condition := getPodCondition(&status, v1.PodReady)
+	return condition
+}
+
+// GetPodCondition extracts the provided condition from the given status and returns that.
+// Returns nil and -1 if the condition is not present, and the index of the located condition.
+func getPodCondition(status *v1.PodStatus, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if status == nil {
+		return -1, nil
+	}
+	return getPodConditionFromList(status.Conditions, conditionType)
+}
+
+// GetPodConditionFromList extracts the provided condition from the given list of condition and
+// returns the index of the condition and the condition. Returns -1 and nil if the condition is not present.
+func getPodConditionFromList(conditions []v1.PodCondition, conditionType v1.PodConditionType) (int, *v1.PodCondition) {
+	if conditions == nil {
+		return -1, nil
+	}
+	for i := range conditions {
+		if conditions[i].Type == conditionType {
+			return i, &conditions[i]
+		}
+	}
+	return -1, nil
+}
