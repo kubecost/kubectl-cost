7a1f13faa44302532d58ab00242bedc71c1a9971
Pierluigi Lenoci <pierluigi.lenoci@gmail.com>
2026-03-18 23:39:44 +0100

    Add savings subcommand for request sizing recommendations
    Adds `kubectl cost savings` which queries the Kubecost
    /model/savings/requestSizingV2 API and displays container-level
    request sizing recommendations with estimated monthly savings.
    
    Closes #84
    
    Signed-off-by: Pierluigi Lenoci <pierluigi.lenoci@gmail.com>

---
+++ pkg/cmd/display/savings.go
@@ -0,0 +1,97 @@
+package display
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
+
+	"github.com/kubecost/kubectl-cost/pkg/query"
+)
+
+func WriteSavingsTable(out io.Writer, recs []query.RequestSizingRecommendation, currencyCode string) {
+	t := MakeSavingsTable(recs, currencyCode)
+	t.SetOutputMirror(out)
+	t.Render()
+}
+
+func MakeSavingsTable(recs []query.RequestSizingRecommendation, currencyCode string) table.Writer {
+	t := table.NewWriter()
+
+	style := table.StyleLight
+	style.Options.SeparateColumns = false
+	style.Options.DrawBorder = false
+	style.Options.SeparateHeader = true
+	style.Title.Colors = append(style.Title.Colors, text.Bold)
+	t.SetStyle(style)
+
+	t.SetColumnConfigs([]table.ColumnConfig{
+		{Name: "Namespace", Align: text.AlignLeft},
+		{Name: "Controller", Align: text.AlignLeft, WidthMax: 40, WidthMaxEnforcer: text.WrapSoft},
+		{Name: "Container", Align: text.AlignLeft},
+		{Name: "Current CPU", Align: text.AlignRight},
+		{Name: "Rec. CPU", Align: text.AlignRight},
+		{Name: "Current RAM", Align: text.AlignRight},
+		{Name: "Rec. RAM", Align: text.AlignRight},
+		{Name: "CPU Eff.", Align: text.AlignRight},
+		{Name: "RAM Eff.", Align: text.AlignRight},
+		{
+			Name:  "Savings/mo",
+			Align: text.AlignRight,
+			TransformerFooter: func(val interface{}) string {
+				if f, ok := val.(float64); ok {
+					return fmt.Sprintf("%.2f %s", f, currencyCode)
+				}
+				if s, ok := val.(string); ok {
+					return s
+				}
+				return ""
+			},
+		},
+	})
+
+	t.AppendHeader(table.Row{
+		"Namespace",
+		"Controller",
+		"Container",
+		"Current CPU",
+		"Rec. CPU",
+		"Current RAM",
+		"Rec. RAM",
+		"CPU Eff.",
+		"RAM Eff.",
+		"Savings/mo",
+	})
+
+	totalSavings := 0.0
+	for _, rec := range recs {
+		controller := fmt.Sprintf("%s/%s", rec.ControllerKind, rec.ControllerName)
+		monthlySavings := rec.MonthlySavings.CPU + rec.MonthlySavings.Memory
+		totalSavings += monthlySavings
+
+		t.AppendRow(table.Row{
+			rec.Namespace,
+			controller,
+			rec.ContainerName,
+			rec.LatestKnownRequest.CPU,
+			rec.RecommendedRequest.CPU,
+			rec.LatestKnownRequest.Memory,
+			rec.RecommendedRequest.Memory,
+			fmt.Sprintf("%.0f%%", rec.CurrentEfficiency.CPU*100),
+			fmt.Sprintf("%.0f%%", rec.CurrentEfficiency.Memory*100),
+			fmt.Sprintf("%.2f %s", monthlySavings, currencyCode),
+		})
+	}
+
+	t.AppendFooter(table.Row{
+		"TOTAL", "", "", "", "", "", "", "", "",
+		totalSavings,
+	})
+
+	t.SortBy([]table.SortBy{
+		{Name: "Savings/mo", Mode: table.DscNumeric},
+	})
+
+	return t
+}
